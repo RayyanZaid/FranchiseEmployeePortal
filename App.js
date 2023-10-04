0428aefc47bfd35e035324ceb992c1f1c1e62f2c
Rayyan Zaid <rayyanzaid0401@gmail.com>
2023-10-04 12:46:38 -0700

    experimenting

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Platform, SafeAreaView, Image, Alert, Button } from "react-native";
 
 export default function App() {
+  console.log("App Executed");
+
+  let x = 1;
+  x.toString();
+
+  console.log(require('./assets/logo.png'))
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="String" color="orange" 
+      onPress={() => Alert.alert("My title" , "My message" ,[
+        {text: "Yes"} , {text: "No"}
+      ] )}></Button>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
