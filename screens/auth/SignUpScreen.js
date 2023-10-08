3c78bb9012fe892a649effffd2cfc80673b8a64b
RayyanZaid <rayyanzaid0401@gmail.com>
2023-10-08 00:31:41 -0700

    Connected React Native to Django

--- screens/auth/SignUpScreen.js
+++ screens/auth/SignUpScreen.js
@@ -1,33 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import global_text_styles from "../../global_text_styles";
 import global_button_styles from "../../global_button_styles";
 const SignUpScreen = () => {
-  const [role, onSelectedRole] = React.useState("");
+  const [role, onSelectedRole] = useState("");
+
+  const [name, setName] = useState("");
+
+  const sendRequest = async () => {
+    try {
+      const response = await fetch(
+        `http://192.168.4.45:8080/playground/hello/`,
+        {
+          method: "GET", // Change to GET method
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        const nameFromDjango = data["name"];
+        setName(nameFromDjango);
+      } else {
+        console.error("Failed to send request");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={global_text_styles.titleText}>Sign Up</Text>
 
       <TouchableOpacity
         style={global_button_styles.signUpButton}
-        onPress={() => console.log("Employee")}
+        onPress={() => {
+          onSelectedRole("Employee");
+          sendRequest();
+        }}
       >
         <Text style={global_button_styles.buttonText}>Employee</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
         style={global_button_styles.signUpButton}
-        onPress={() => console.log("Manager")}
+        onPress={() => {
+          onSelectedRole("Manager");
+          sendRequest();
+        }}
       >
         <Text style={global_button_styles.buttonText}>Manager</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
         style={global_button_styles.signUpButton}
-        onPress={() => console.log("Admin")}
+        onPress={() => {
+          onSelectedRole("Admin");
+          sendRequest();
+        }}
       >
         <Text style={global_button_styles.buttonText}>Admin</Text>
       </TouchableOpacity>
+
+      <Text>{name}</Text>
     </View>
   );
 };
@@ -37,7 +75,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff", // Background color
+    backgroundColor: "#fff",
   },
 });
 
